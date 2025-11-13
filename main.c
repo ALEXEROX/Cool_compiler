bec390a274e60e6483b5fc9c822dfb37887514e2
Alexei Ermakov <ermakov@alexerox.ru>
2025-11-13 19:26:42 +0300

    Добавлена возможность выбрать компилируемый файл через аргумент или ввод в консоль

--- main.c
+++ main.c
@@ -11,15 +11,18 @@ extern ProgramNode *program;
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "Russian");
-    // char *filename;
-    // if (argc == 1) {
-    //     filename = argv[0];
-    // }
-    // else {
-    //     printf("Enter full file name");
-    //     scanf("%s", filename);
-    // }
-    yyin = fopen("source.cl", "r");
+
+    char filename[255];
+
+    if (argc == 2) {
+        strcpy(filename, argv[1]);
+    }
+    else {
+        printf("Enter file name\n");
+        scanf("%s", &filename);
+    }
+
+    yyin = fopen(filename, "r");
     yyparse();
     save_ast_dot(program);
     system("Dot/dot.exe Dot/cool_dot.dot -Tsvg > dot.svg");
